56f0f7bbcd46607ce4b960fe566827d35db8ca31
nhan <longnhan252@gmail.com>
2024-06-25 12:23:51 +0700

    check null for arguments, fix ioctl() warning

--- peripheral_Driver/bb_i2c.c
+++ peripheral_Driver/bb_i2c.c
@@ -1,42 +1,75 @@
 #include "bb_i2c.h"
 
-void bb_i2cInit(struct i2c_st *ptr)
+void bb_i2cInit(i2c_st *ptr)
 {
-    /*Open the I2C bus*/
-    if((ptr->i2c_fd = open(ptr->i2c_char, O_RDWR)) < 0) 
+    if(ptr == NULL)
     {
-        fprintf(stderr, "Failed to open the bus\n");
+        fprintf(stderr, "NULL\n");
         exit(EXIT_FAILURE);
     }
-    /*Set the I2C slave address*/
-    if(ioctl(ptr->i2c_fd, I2C_SLAVE, ptr->slave_addr) < 0) 
+    else
     {
-        fprintf(stderr, "Failed to acquire bus access and/or talk to slave\n");
-        exit(EXIT_FAILURE);
+        /*Open the I2C bus*/
+        if((ptr->i2c_fd = open(ptr->i2c_char, O_RDWR)) < 0) 
+        {
+            fprintf(stderr, "Failed to open the bus\n");
+            exit(EXIT_FAILURE);
+        }
+        /*Set the I2C slave address*/
+        if(ioctl(ptr->i2c_fd, I2C_SLAVE, ptr->slave_addr) < 0) 
+        {
+            fprintf(stderr, "Failed to acquire bus access and/or talk to slave\n");
+            close(ptr->i2c_fd);
+            exit(EXIT_FAILURE);
+        }
     }
 }
 
-void bb_i2cClose(struct i2c_st *ptr)
+void bb_i2cClose(i2c_st *ptr)
 {
-    close(ptr->i2c_fd);
+    if(ptr == NULL)
+    {
+        fprintf(stderr, "NULL\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        close(ptr->i2c_fd);
+    }
 }
 
-uint8_t bb_getI2cData(struct i2c_st *ptr, uint8_t regAddress, uint8_t *buf, uint8_t size)
+uint8_t bb_i2c_master_getData(i2c_st *ptr, uint8_t *regAddress, uint8_t *buf, uint8_t size)
 {
     uint8_t ret = 0;
-
-    write(ptr->i2c_fd, &regAddress, 1);
-    ret = read(ptr->i2c_fd, buf, size);
+    if((ptr == NULL) || (regAddress == NULL) || (buf == NULL))
+    {
+        fprintf(stderr, "NULL\n");
+        close(ptr->i2c_fd);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        write(ptr->i2c_fd, regAddress, size);
+        ret = read(ptr->i2c_fd, buf, size);
+    }
     return ret;
 }
 
-uint8_t bb_sendI2cData(struct i2c_st *ptr, uint8_t regAddress, uint8_t cmd)
+uint8_t bb_i2c_master_sendData(i2c_st *ptr, uint8_t regAddress, uint8_t cmd)
 {
     uint8_t ret = 0;
-    uint8_t send_Buf[2]={0};
-    send_Buf[0] = regAddress;
-    send_Buf[1] = cmd;
-
-    ret = write(ptr->i2c_fd, send_Buf, 2);
+    if(ptr == NULL)
+    {
+        fprintf(stderr, "NULL\n");
+        close(ptr->i2c_fd);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        uint8_t send_Buf[2]={0};
+        send_Buf[0] = regAddress;
+        send_Buf[1] = cmd;
+        ret = write(ptr->i2c_fd, send_Buf, 2);
+    }
     return ret;
 }
--- peripheral_Driver/bb_i2c.h
+++ peripheral_Driver/bb_i2c.h
@@ -2,7 +2,7 @@
 #define _BB_I2C_H_
 
 #include <linux/i2c-dev.h>
-#include <linux/mmc/ioctl.h>
+#include <sys/ioctl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,17 +20,17 @@
 #ifdef BBB_BOARD
 #endif /*BBB_BOARD*/
 
-typedef struct i2c_st
+typedef struct
 {
     int i2c_fd;
     char *i2c_char;
     uint8_t slave_addr;
-};
+}i2c_st;
 
 
-void bb_i2cInit(struct i2c_st *ptr);
-void bb_i2cClose(struct i2c_st *ptr);
-uint8_t bb_getI2cData(struct i2c_st *ptr, uint8_t regAddress, uint8_t *buf, uint8_t size);
-uint8_t bb_sendI2cData(struct i2c_st *ptr, uint8_t regAddress, uint8_t cmd);
+void bb_i2cInit(i2c_st *ptr);
+void bb_i2cClose(i2c_st *ptr);
+uint8_t bb_i2c_master_getData(i2c_st *ptr, uint8_t *regAddress, uint8_t *buf, uint8_t size);
+uint8_t bb_i2c_master_sendData(i2c_st *ptr, uint8_t regAddress, uint8_t cmd);
 
 #endif /*_BB_I2C_H_*/
